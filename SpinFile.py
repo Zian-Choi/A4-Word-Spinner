88efe0fda6091f5b14fa4d4d7bdb8355068d8e14
puagra <choi.inyoung05@gmail.com>
2024-10-24 21:37:53 +0900

    Second commit for SpinFIle

--- SpinFile.py
+++ SpinFile.py
@@ -8,3 +8,11 @@ def main():
         original_text = file.read().lower()
 
     print("Original:", original_text)
+
+    for i in range(1, 4):
+        modified_text = spinner.spin_text(original_text)
+        print(f"Option {i}", modified_text)
+
+
+if __name__ == "__main__":
+    main()
